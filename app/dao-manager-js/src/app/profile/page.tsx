c2ce454a714735283450cdb23860c21d142abb5f
unknown <vballiev@gmail.com>
2024-11-19 18:42:59 +0200

    add(lib): add privet key auth

--- app/dao-manager-js/src/app/profile/page.tsx
+++ app/dao-manager-js/src/app/profile/page.tsx
@@ -1,36 +1,35 @@
 "use client";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 import DaoManagerJS from "../../../../../package/dao_manager_js_lib";
 import CustomButton from "../../shared_widgets/custom_button";
 import { useRouter } from "next/navigation";
-import { NetworkID } from "../../../../../package/models/near_models";
+import {
+  ConnectionType,
+  NetworkID,
+} from "../../../../../package/models/near_models";
+import { UrlDashboard } from "../../url_dashboard/url_dashboard";
 
 export default function Profile() {
   const router = useRouter();
+  const typeConnection = localStorage.getItem("connection");
+  const network = localStorage.getItem("network");
   const daoManagerJS = DaoManagerJS.getInstance();
 
-  const network = localStorage.getItem("network");
-  if (network == "mainnet") {
-    daoManagerJS.createWalletConnection({ networkID: NetworkID.mainnet });
-  } else if (network == "testnet") {
-    daoManagerJS.createWalletConnection({ networkID: NetworkID.testnet });
-  } else {
-    throw new Error("You need login");
+  if (!typeConnection) {
+    router.push(UrlDashboard.login);
+  } else if (typeConnection == "wallet") {
+    daoManagerJS.createConnection({
+      connectionType: ConnectionType.wallet,
+      networkID: network == "mainnet" ? NetworkID.mainnet : NetworkID.testnet,
+    });
+  } else if (typeConnection == "default") {
   }
-
-  useEffect(() => {
-    const res = daoManagerJS.nearWallet.checkIsSign();
-    console.log(res);
-  }, [daoManagerJS]);
 
   function logOut() {
     daoManagerJS.signOut();
     localStorage.removeItem("network");
-    router.push("/login");
-  }
-
-  async function test() {
-    await daoManagerJS.nearWallet.test();
+    localStorage.removeItem("connection");
+    router.push(UrlDashboard.login);
   }
 
   return (
@@ -39,12 +38,6 @@ export default function Profile() {
         text="Log Out"
         onClick={() => {
           logOut();
-        }}
-      />
-      <CustomButton
-        text="test"
-        onClick={async () => {
-          await test();
         }}
       />
     </div>
